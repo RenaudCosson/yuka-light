349e51ec9e088b0575569d650eaee73345b9e061
renaud cosson <renaud.cosson@fabernovel.com>
2023-07-04 11:10:11 +0200

    init commit

---
+++ YukaLike/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  YukaLike
+//
+//  Created by Pierre Cheylus on 14/11/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
